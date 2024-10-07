d699f05e4c32bd4e06436f70d03ebfeffb746ce1
Maésha Mahmud <83089310+mash3133@users.noreply.github.com>
2024-10-07 14:12:49 -0400

    new design

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
 window.addEventListener('scroll', function() {
     let scrollPosition = window.scrollY;
     let header = document.querySelector('header');
